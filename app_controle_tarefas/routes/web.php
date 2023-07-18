eb815fc899cae98b0becf74d67eaf9bb08817eff
NivaldoNeto <nivaldoveieiranto@gmail.com>
2023-07-18 17:52:37 -0300

    bootstrap 222

--- app_controle_tarefas/routes/web.php
+++ app_controle_tarefas/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Mail\MensagemTesteMail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +23,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController')->middleware('auth');
+
+Route::get('mensagem-teste', function() {
+    return new MensagemTesteMail();
+    //Mail::to('hdnivans@gmail.com')->send(new MensagemTesteMail());
+    //return ' E-mail enviado com sucesso!';
+});
